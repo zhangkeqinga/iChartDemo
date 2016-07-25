8b6e50779ea85838edb55b3fe80a73cb8a8a0bb9
Francis-light <hollywinter@126.com>
2016-07-25 09:28:21 +0800

    Initial Commit

---
+++ iChartDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iChartDemo
+//
+//  Created by mac on 16/7/25.
+//  Copyright © 2016年 com.cn.QZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
